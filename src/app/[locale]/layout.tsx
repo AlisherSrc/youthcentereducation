dbad09010c98be59487f09b6c645899780cd56fc
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-08-06 18:10:35 +0600

    adding vercek analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { useLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { useTranslations } from "next-intl";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -55,6 +56,7 @@ export default function RootLayout({
           }
         } />
           {children}
+          <Analytics />
       </body>
     </html>
   )
